6f7e1cd43132e52e689803fdaeefe0af1027e02d
Dimitri Glazkov <dimitri@glazkov.com>
2022-10-02 11:03:48 -0700

    Added API documentation and handling of CORS

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-import replicate
-import openai
+import json
+import os
 import traceback
 
-import os
-import json
-from flask import Flask, jsonify, request
-# from google.cloud import secretmanager
+import openai
+import replicate
 from dotenv import load_dotenv
+from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
 
@@ -16,28 +15,38 @@ openai.api_key = os.getenv("OPENAI_API_TOKEN")
 
 @app.route("/api/stable-diffusion", methods=["POST"])
 def stable_diffusion():
-    prompt = json.loads(request.data)["prompt"]
+    prompt = request.form["prompt"]
+
     if prompt is None:
         return jsonify({
             "error": 'No prompt supplied'
         })
-    try: 
-        replicate_client = replicate.Client(api_token=os.getenv("REPLICATE_API_TOKEN"))
+    try:
+        replicate_client = replicate.Client(
+            api_token=os.getenv("REPLICATE_API_TOKEN"))
         model = replicate_client.models.get("stability-ai/stable-diffusion")
         image_url = model.predict(prompt=prompt)[0]
-        return jsonify({ 
+        response = jsonify({
             "prompt": prompt,
-            "image_url": image_url 
+            "image_url": image_url
         })
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        return response
+
     except Exception as e:
         return jsonify({
             "error": f"{e}\n{traceback.print_exc()}"
         })
-        
+
+
+@app.route("/api/stable-diffusion", methods=["GET"])
+def stable_diffusion_sample():
+    return render_template("stable-diffusion-sample.html")
+
 
 @app.route("/api/gpt-3", methods=["POST"])
 def gpt_3():
-    prompt = json.loads(request.data)["prompt"]
+    prompt = request.form["prompt"]
     if prompt is None:
         return jsonify({
             "error": 'No prompt supplied'
@@ -52,14 +61,23 @@ def gpt_3():
             frequency_penalty=0,
             presence_penalty=0
         )
-        return jsonify({ 
+        response = jsonify({
             "prompt": prompt,
             "text": response.choices[0].text
         })
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        return response
+
     except Exception as e:
         return jsonify({
             "error": f"{e}\n{traceback.print_exc()}"
         })
+
+
+@app.route("/api/gpt-3", methods=["GET"])
+def gpt_3_sample():
+    return render_template("gpt-3-sample.html")
+
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8080, debug=True)
