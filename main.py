5f96d11bb2d73f48a31ce0358f341f3780b000c0
Dimitri Glazkov <dimitri@glazkov.com>
2022-11-03 21:22:31 -0700

    Added uploading DALL-E images to GCS bucket.

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
+import urllib.request
 import fnmatch
 import os
 import re
 import traceback
+import uuid
 
 import openai
 import replicate
 from dotenv import load_dotenv
 from flask import Flask, jsonify, render_template, request
 from flask_cors import CORS
+from google.cloud import storage
 
 origins = []
 
@@ -24,6 +27,22 @@ CORS(app, supports_credentials=True, origins=origins)
 
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_TOKEN")
+
+bucket_name = 'ai-sampler-generated-images'
+storage_client = storage.Client()
+
+
+def upload_generated_image(image_url):
+    file = urllib.request.urlopen(image_url)
+
+    filename = f"{uuid.uuid4()}.png"
+
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(filename)
+
+    blob.upload_from_string(file.read(), content_type='image/png')
+
+    return f"https://storage.googleapis.com/{bucket_name}/{filename}"
 
 
 @app.route("/api/stable-diffusion", methods=["POST"])
@@ -102,10 +121,10 @@ def dall_e():
             prompt=prompt,
             n=1,
             size="512x512"
-        )
+        )        
         return jsonify({
             "prompt": prompt,
-            "image_url": response['data'][0]['url']
+            "image_url": upload_generated_image(response['data'][0]['url'])
         })
 
     except Exception as e:
